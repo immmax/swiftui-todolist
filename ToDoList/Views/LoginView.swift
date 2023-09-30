a289fcef0cb750cf176464a62644a51e43edc3bb
Maxim Datskiy <datskiumax@gmail.com>
2023-09-30 18:31:10 -0500

    Update LoginView
    Put data to LoginViewViewModel
    Add verification to email and password
    Connect to Firebase

--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -8,8 +8,48 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @StateObject var viewModel = LoginViewViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // Header
+            HeaderView(title: "To Do List",
+                       subtitle: "Get things done",
+                       angle: 15,
+                       background: Color.pink)
+            
+            Form {
+                
+                if !viewModel.errorMessage.isEmpty {
+                    Text(viewModel.errorMessage)
+                        .foregroundStyle(.red)
+                }
+                
+                TextField("Email Address", text: $viewModel.email)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocapitalization(.none)
+                    .autocorrectionDisabled()
+                SecureField("Password", text: $viewModel.password)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                
+                TLButton(title: "Log In", background: .blue) {
+                    viewModel.login()
+                }
+                .padding()
+            }
+            .offset(y: -50)
+            
+            //Create Account
+            VStack {
+                Text("New around here?")
+                
+                NavigationLink("Create an account", destination: RegisterView())
+            }
+//            .padding(.bottom, 50)
+            
+            Spacer()
+        }
     }
 }
 
