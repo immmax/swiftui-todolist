17113058c089d586f41bdc223390095e349780e3
Maxim Datskiy <datskiumax@gmail.com>
2023-09-30 18:32:18 -0500

    Update RegisterView
    Create registration form

--- ToDoList/Views/RegisterView.swift
+++ ToDoList/Views/RegisterView.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct RegisterView: View {
+    
+    @State private var name = ""
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // Header
+            HeaderView(title: "Register",
+                       subtitle: "Start organizzing todos",
+                       angle: -15,
+                       background: .orange)
+            
+            Form {
+                TextField("Full Name", text: $name)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocorrectionDisabled()
+                TextField("Email Address", text: $email)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocapitalization(.none)
+                    .autocorrectionDisabled()
+                TextField("Password", text: $password)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                
+                TLButton(title: "Create Account", background: .green) {
+                    // Attempt Registration
+                }
+                .padding()
+            }
+            .offset(y: -50)
+            
+            Spacer()
+        }
     }
 }
 
